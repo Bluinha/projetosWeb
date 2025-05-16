16fcb0eb12597024337c2b58734ba5a8eb0c4ad0
Helena <146356403+Bluinha@users.noreply.github.com>
2025-05-16 15:48:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("growButton").addEventListener('click',growFlower)
+
+growFlower(){
+    const frames = [
+        'frame01',
+        'frame02',
+        'frame03',
+        'frame04',
+        'frame05'
+    ]
+}
